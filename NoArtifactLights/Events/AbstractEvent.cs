17c2b5279576d43a040726fc419a372fa958e8e5
man Ender <EndermanLOTR@outlook.com>
2020-08-26 20:27:15 +0800

    feat(event): add stealcar event
    adds another event!

---
+++ NoArtifactLights/Events/AbstractEvent.cs
@@ -0,0 +1,24 @@
+﻿using GTA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoArtifactLights.Events
+{
+    public abstract class AbstractEvent
+    {
+        public virtual Ped EventPed { get; }
+
+        public abstract void ProceedLogicOnce(Ped p);
+        public abstract void Initialize(Ped p);
+        public virtual void End()
+        {
+            if(EventPed != null && EventPed.Exists())
+            {
+                EventPed.MarkAsNoLongerNeeded();
+            }
+        }
+    }
+}
---
+++ NoArtifactLights/Events/StealCar.cs
@@ -0,0 +1,32 @@
+﻿using GTA;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoArtifactLights.Events
+{
+    public class StealCar : IEvent
+    {
+        Blip b;
+
+        public void End()
+        {
+            
+        }
+
+        public void Initialize()
+        {
+            
+        }
+
+        public void ProceedLogicOnce(Ped p)
+        {
+            p.Task.EnterAnyVehicle(VehicleSeat.Driver, -1, 1, EnterVehicleFlags.AllowJacking);
+            b = p.AddBlip();
+            b.IsFlashing = true;
+            b.Sprite = BlipSprite.PointOfInterest;
+        }
+    }
+}
